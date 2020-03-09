5a719a8d6bb67a5b10aaa3fe2585bab1a07fc760
Amanda <jkim@lcs.on.ca>
2020-03-09 11:23:49 -0400

    Guard statements put in and clears error message in the beginning

--- DeficientPerfectAbundant/ViewController.swift
+++ DeficientPerfectAbundant/ViewController.swift
@@ -17,9 +17,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Clear errormessage
+        errorMessage.text = ""
     }
+    
     @IBAction func check(_ sender: Any) {
+        guard let numberInput = Int(input.text!) else {
+            errorMessage.text = "Please enter an integer"
+            return
+        }
         
+        guard numberInput > 1, numberInput < 32500 else {
+            errorMessage.text = "Enter a number between 1 and 32500"
+            return
+        }
     }
     
 }
