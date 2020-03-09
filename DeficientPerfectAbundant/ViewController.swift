b867b420b651427c88e97d758b55c187c026af10
Amanda <jkim@lcs.on.ca>
2020-03-09 11:05:55 -0400

    Initial Commit

---
+++ DeficientPerfectAbundant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeficientPerfectAbundant
+//
+//  Created by Kim, Jenis on 2020-03-09.
+//  Copyright © 2020 Kim, Jenis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
